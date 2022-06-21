752e3eec2771951c1cca052c12de348c2a7fa57c
stayhigh <redhero0702@gmail.com>
2022-06-21 14:59:47 +0800

    add err check for installer

--- fzf/fzf_installer.sh
+++ fzf/fzf_installer.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-yes | apt install fzf
-yes | apt install ripgrep
+# fzf: fuzzy finder
+# ripgrep: fast grep
+# bat: syntax highlight, https://github.com/sharkdp/bat/blob/master/doc/README-zh.md
 
-# https://github.com/sharkdp/bat/blob/master/doc/README-zh.md
-yes | apt install bat
+apt install -y fzf ripgrep bat
+
+if [[$? -ne 0]]; then
+  exit -1
+fi
+
 ln -s /usr/bin/batcat /usr/local/bin/bat # for ubuntu
+ln -s rfv /usr/local/bin/rfv
 
 echo alias pf="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}" >> ~/.bashrc
 source ~/.bashrc
